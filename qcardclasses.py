2d66727b03ea2d026e8d6aa9a603a23a8762c379
ernst <rfanfan@gmail.com>
2020-11-07 11:20:35 -0500

    Add Card class getter, setter and repr

--- qcardclasses.py
+++ qcardclasses.py
@@ -2,8 +2,27 @@
 #back end classes for qcared to work###############################################
 ###################################################################################
 
+#card manager
 class Card():
-    pass
+    def __init__(self, question=None, answer=None):
+        self.question = question
+        self.answer = answer
+        
+    def get_question(self):
+        return self.question
+        
+    def get_answer(self):
+        return self.answer
+        
+    def set_question(self, question):
+        self.question = question
+        
+    def set_answer(self, answer):
+        self.answer = answer
+        
+    def __repr__(self):#may need mod for jason DB
+        message = "{}, {}".format(self.question, self.answer)
+        return message
     
 class Set_of_Cards():
     pass
