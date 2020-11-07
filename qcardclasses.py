8a9f3d4ad7c03acda5e3502073660eb77a4ff16d
ernst <rfanfan@gmail.com>
2020-11-07 12:23:05 -0500

    Add Set of Card functions

--- qcardclasses.py
+++ qcardclasses.py
@@ -1,5 +1,6 @@
 ###################################################################################
 #back end classes for qcared to work###############################################
+#Python 3.9########################################################################
 ###################################################################################
 
 #card manager
@@ -25,7 +26,48 @@ class Card():
         return message
     
 class Set_of_Cards():
-    pass
+    def __init__(self, name=None, subject=None, cards=None):
+        self.name = name
+        self.subject = subject
+        if cards == None:
+            self.cards = {}
+        
+    def set_name(self, name):
+        self.name = name
+        
+    def set_subject(self, subject):
+        self.subject
+        
+    def set_cards(self, cards):
+        self.cards = cards 
+        
+    def get_name(self):
+        return self.name
+        
+    def get_subject(self):
+        return self.subject
+        
+    def get_cards(self):
+        return self.cards
+        
+    def add_card(self, card):
+        key = card.get_question()
+        value = card.get_answer()
+        self.cards[key] = value 
+        
+    def merge_sets(self, new_set, name=None, subject=None):
+        if not name == None:
+            self.set_name(name)
+        if not subject == None:
+            self.set_subject(subject)
+        self.cards += new_set.get_cards()
+        
+    def __add__(self, cards):
+        return self.cards | new_set.get_cards
+        
+    def __repr__(self):
+        msg = "{}, {}, {}".format(self.name, self.subject, self.cards)
+        return msg
     
 class Library():
     pass
@@ -35,3 +77,13 @@ class Quiz():
     
 class test(Quiz):
     pass
+    
+#test
+card1 = Card("1+1", "2")
+card2 = Card("1+2", "3")
+print (card1)
+print(card2)
+set1 = Set_of_Cards("One table", "Adding")
+set1.add_card(card1)
+set1.add_card(card2)
+print(set1)
