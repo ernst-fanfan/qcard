b858bdd16edbf1535cf5ec031a838aa63a504b5e
ernst <rfanfan@gmail.com>
2020-11-09 01:00:25 -0500

    Ignore json file Extended json encoder to handle new classes Updated all __init__() Updated all __repr__() Update pull and push to json functions Tested and compiles

--- qcardclasses.py
+++ qcardclasses.py
@@ -5,10 +5,22 @@
 
 import json
 
+#to help json encoder process my classes
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Library):
+            return obj.library
+        elif isinstance(obj, Set_of_Cards):
+            return obj.structure
+        elif isinstance(obj, Card):
+            return obj.content
+        else:
+            return json.JSONEncoder.default(self, obj)
+
 #card manager
 class Card():
-    def __init__(self, question=None, answer=None):
-        self.content = {'index' : None, 'question' : question, 'answer' : answer}
+    def __init__(self, index = None, question=None, answer=None):
+        self.content = {'index' : index, 'question' : question, 'answer' : answer}
         
     def get_question(self):
         return self.content['question']
@@ -29,11 +41,14 @@ class Card():
         self.content['index'] = index
         
     def __repr__(self):#may need mod for jason DB
-        return self.content
+        return str(self.content)
     
 class Set_of_Cards():
-    def __init__(self, name=None, subject=None, cards={}):
-        self.structure = {'index' : None, 'name' : name, 'subject' : subject, 'size' : 0, 'cards' : cards}
+    def __init__(self, index=None, name=None, subject=None, size=0, cards=None):
+        if cards == None:
+            self.structure = {'index' : index, 'name' : name, 'subject' : subject, 'size' : size, 'cards' : {}}
+        else:
+            self.structure = {'index' : index, 'name' : name, 'subject' : subject, 'size' : size, 'cards' : cards}
         
     def set_name(self, name):
         self.structure['name'] = name
@@ -58,7 +73,7 @@ class Set_of_Cards():
         return self.structure['subject']
         
     def get_cards(self):
-        return self.structure.['cards']
+        return self.structure['cards']
 
     def get_size(self):
         return self.structure['size']
@@ -105,11 +120,14 @@ class Set_of_Cards():
         self.set_size(index)
         
     def __repr__(self):
-        return self.structure
+        return str(self.structure)
 
 class Library():
-    def __init__(self, owner, size=0, library={}):
-        self.library = {'owner' : owner, 'size' : size,'library' : library}
+    def __init__(self, owner, size=0, library=None):
+        if library == None:
+            self.library = {'owner' : owner, 'size' : size,'library' : {}}
+        else:
+            self.library = {'owner' : owner, 'size' : size,'library' : library}
 
     def get_owner(self):
         return self.library['owner']
@@ -152,19 +170,32 @@ class Library():
         self.set_size(index)
 
     def __repr__(self):
-        return self.library
+        return str(self.library)
 
     #File management
     def push_to_file(self):
-        to_dump = self.library
+        library = self
         with open("library.json", "w") as jfile:
-            json.dump(to_dump, jfile)
+            json.dump(library, jfile, cls=MyEncoder)
         self.library = None
 
     def pull_from_file(self):
         with open("Library.json") as jfile:
             data = json.load(jfile)
-        self.library = data
+            self.set_size(data['size'])
+            for key in data['library']:
+                #rebuild set
+                set_ = data['library'][key]
+                extr_set = Set_of_Cards(index=set_['index'], name=set_['name'], subject=set_['subject'], size=set_['size'])
+                
+                for key2 in set_['cards']:
+                    #rebuild card
+                    card = set_['cards'][key2] 
+                    extr_card = Card(index=card['index'], question=card['question'], answer=card['answer'])
+                    extr_set.structure['cards'][key2] = extr_card
+
+                self.library['library'][key] = extr_set
+                
     
 class Quiz():
     def __init__(self, new_set):
@@ -185,34 +216,48 @@ card1 = Card("1+1", "2")
 card2 = Card("1+2", "3")
 card3 = Card("1+3", "4")
 card4 = Card("1+4", "5")
-
-set1 = Set_of_Cards("One table", "Adding")
-set1.add_card(card1)
-set1.add_card(card2)
-set1.add_card(card3)
-set1.add_card(card4)
-
-#set1.remove_card(card1)
 card5 = Card("1st President", "George Washignton")
 card6 = Card("46th President", "Joe Biden")
 card7 = Card("44th President", "Barrak Obama")
 card8 = Card("43rd President", "George W Bush jr")
 
+set1 = Set_of_Cards("One table", "Adding")
 set2 = Set_of_Cards("Presidents", "History")
+
+print(set1)
+print()
+print(set2)
+print()
+
+set1.add_card(card1)
+set1.add_card(card2)
+set1.add_card(card3)
+set1.add_card(card4)
 set2.add_card(card5)
 set2.add_card(card6)
 set2.add_card(card7)
 set2.add_card(card8)
+
+print(set1)
+print()
+print(set2)
+print()
+
+
+
 
 lib1 = Library("mee")
 lib1.add_set(set1)
 lib1.add_set(set2)
 
 print(lib1)
+print()
 
 lib1.push_to_file()
 print(lib1)
 
+lib1 = Library("mee")
 lib1.pull_from_file()
 print(lib1)
-
+print()
+# print(lib1.get_library())
