52699b95aa50bfbb999a5257e6a81ff32d98ad6c
ernst <rfanfan@gmail.com>
2020-11-11 19:09:33 -0500

    Update test.py
    Fully tested

--- test.py
+++ test.py
@@ -1,53 +1,46 @@
+from qcardclasses import Card, Deck, Library
+import appmethods as app
 #test
 card1 = Card(question="1+1", answer="2")
 card2 = Card(question="1+2", answer="3")
 card3 = Card(question="1+3", answer="4")
 card4 = Card(question="1+4", answer="5")
-# card5 = Card("1st President", "George Washignton")
-# card6 = Card("46th President", "Joe Biden")
-# card7 = Card("44th President", "Barrak Obama")
-# card8 = Card("43rd President", "George W Bush jr")
+card5 = Card("1st President", "George Washignton")
+card6 = Card("46th President", "Joe Biden")
+card7 = Card("44th President", "Barrak Obama")
+card8 = Card("43rd President", "George W Bush jr")
 
-set1 = Deck(name="One table", subject="Adding")
-# set2 = Deck("Presidents", "History")
+deck1 = Deck(name="One table", subject="Adding")
+deck2 = Deck("Presidents", "History")
 
-# print(set1)
+deck1.add_card(card1)
+deck1.add_card(card2)
+deck1.add_card(card3)
+deck1.add_card(card4)
+deck2.add_card(card5)
+deck2.add_card(card6)
+deck2.add_card(card7)
+deck2.add_card(card8)
+
+lib1 = Library("mee")
+lib1.add_deck(deck1)
+lib1.add_deck(deck2)
+
+# print(card1)
 # print()
-# print(set2)
+# print(deck1)
 # print()
-
-set1.add_card(card1)
-set1.add_card(card2)
-set1.add_card(card3)
-set1.add_card(card4)
-# set2.add_card(card5)
-# set2.add_card(card6)
-# set2.add_card(card7)
-# set2.add_card(card8)
-
-# print(set1)
+# print(deck2)
 # print()
-# print(set2)
-# print()
-
-
-
-
-lib1 = decks("mee")
-lib1.add_set(set1)
-# lib1.add_set(set2)
-
-print(lib1)
-print()
-
-# lib1.push_to_file()
-# print(lib1)
-
-# lib1 = decks("mee")
-# lib1.pull_from_file()
 # print(lib1)
 # print()
-# # print(lib1.get_decks())
-selected_set = lib1.get_set(1)
-quiz1 = Quiz(selected_set, 3)
-print(quiz1)
+
+app.save_library(lib1)
+lib2 = app.open_library("library.json")
+# print(lib2)
+
+lib2.merge_libraries(lib1)
+# print(lib2)
+
+deck1.merge_decks(deck2, 'Adding and Presidents', 'Math and History')
+print(deck1)
